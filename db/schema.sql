f52e4ed3f56fefb5e8e2ecdb4b263d73b96a917d
khalil <kamin86@gmail.com>
2018-03-28 17:45:38 -0400

    initial setup

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+USE DATABASE burgers_db;
+CREATE TABLE burgers(
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(100),
+  devoured DATATYPE BOOLEAN,
+  PRIMARY KEY (ID)
+);
